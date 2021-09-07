49d30cacba6d8b847a2bb204b29b9e0e982b9d56
ha308ing <kadined37-y9sb2dpbi55Y@yahoo.com>
2021-09-08 01:20:26 +0300

    demove unparallel dependencies

--- src/RepoMenu.js
+++ src/RepoMenu.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react"
 import { useIterator } from "./hooks/useIterator"
-import RepositoryReadme from "./RepositoryReadme"
 
 export default function RepoMenu( { repositories, onSelect = f => f, login } ) {
   const [ { name }, previous, next ] = useIterator( repositories )
@@ -19,7 +18,6 @@ export default function RepoMenu( { repositories, onSelect = f => f, login } ) {
         <p>{ name }</p>
         { repositories.length > 1 && <button onClick={ next }>&gt;</button> }
       </div>
-      <RepositoryReadme login={ login } repo={ name } />
     </>
   )
 }
