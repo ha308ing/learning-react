a1dd42f56a17036b7af25d8ef57ec23aed99842c
ha308ing <kadined37-y9sb2dpbi55Y@yahoo.com>
2021-09-09 13:18:15 +0300

    match properties names

--- book_examples/UserRepositories.js
+++ book_examples/UserRepositories.js
@@ -2,15 +2,14 @@ import React from "react"
 import Fetch from "./Fetch"
 import RepoMenu from "./RepoMenu"
 
-export default function UserRepositories( { login, selectedRepo, onSelect = f => f } ) {
+export default function UserRepositories( { login, repo, onSelect = f => f } ) {
   return (
     <Fetch
       uri={`https://api.github.com/users/${ login }/repos`}
       renderSuccess={ ( { data } ) =>
         <RepoMenu
           repositories={ data }
-          selectedRepo={ selectedRepo }
-          login = { login}
+          selected={ repo }
           onSelect={ onSelect }
         />
       }
