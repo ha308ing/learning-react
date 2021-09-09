5803f8ff14faff76344271c1ee59f9b9a0db543b
ha308ing <kadined37-y9sb2dpbi55Y@yahoo.com>
2021-09-09 12:51:11 +0300

    handle input. clear repo with clean login conditional rendering

--- book_examples/index.js
+++ book_examples/index.js
@@ -8,7 +8,13 @@ export default function App() {
   const [ login, setLogin ] = useState(  )
   const [ repo, setRepo ] = useState(  )
 
+  const handleSearch = login => {
+    if ( login ) return setLogin(login)
+    setLogin( "" )
+    setRepo( "" )
+  }
 
+  if ( !login ) return <SearchForm value={ login } onSearch={ handleSearch } />
 
   return (
     <>
