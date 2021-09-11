ff7b8c02ea9b44c7608d636fe4bab1b7f841efd8
ha308ing <kadined37-y9sb2dpbi55Y@yahoo.com>
2021-09-11 23:28:18 +0300

    add initial value

--- book_examples/SearchForm.js
+++ book_examples/SearchForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 
-export default function SearchForm( { value, onSearch } ) {
+export default function SearchForm( { value = "", onSearch } ) {
   const [ login_, setLogin ] = useState( value )
 
   return (
